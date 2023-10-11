40d25a4e1c80bea46fe2713dff97a2dafda22360
Samuel <samuel11299ac@gmail.com>
2023-10-10 21:21:16 -0600

    Express funcionando

---
+++ app.mjs
@@ -0,0 +1,15 @@
+//Importación de módulos
+import express from "express";
+
+//Creación de la instancia
+const app = express();
+
+//Declaración de rutas
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//Puerto a escuchar
+app.listen(3000, () => {
+  console.log("Escuchando en el puerto 3000!");
+});
